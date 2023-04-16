35774c712726f33cdc4c571dbd98d22ae4db0cd0
Alessandro Maciel <alessandromaciel237@gmail.com>
2023-04-16 20:02:45 -0300

    atualizando app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Formulario from './components/Formulario';
+import Lista from './components/Lista';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Word</h1>
+      <Formulario />
+      <Lista />
     </div>
   );
 }
